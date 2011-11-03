05ca15f823e43929db3ea4ff923eb8b2a5af4a4c
William Dillon <hpux735@gmail.com>
2011-11-03 15:09:23 -0700

    Corrected copyright notice

--- TCP-audio-OSX/AppDelegate.m
+++ TCP-audio-OSX/AppDelegate.m
@@ -3,7 +3,7 @@
 //  TCP-audio-OSX
 //
 //  Created by William Dillon on 11/3/11.
-//  Copyright (c) 2011 Oregon State University (COAS). All rights reserved.
+//  Copyright (c) 2011. All rights reserved.
 //
 
 #import "AppDelegate.h"
